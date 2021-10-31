893a2408b6f71f3f72904c0fec0b8e708ec2adf7
Rabi León <rabi_leon@hotmail.com>
2021-10-31 11:27:09 -0600

    🚧 clase 14

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    //para insertar el cambio y mover las fuentes a otra carpte lo hacemos aqui
-    assetModuleFilename: 'assets/image/[hash][ext][query]',
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
   resolve: {
     extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    },
   },
   module: {
     rules: [
@@ -33,9 +41,16 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]',
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[contenthash].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
+            esModule: false,
+          },
         },
       },
     ],
@@ -46,7 +61,9 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css',
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -55,5 +72,10 @@ module.exports = {
         },
       ],
     }),
+    new Dotenv(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
